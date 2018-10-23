18e8efbb6407f69d770849af68edd461ca64019b
mrkun <kuntapon55@gmail.com>
2018-10-23 09:48:28 +0700

    Start User Git

---
+++ VarAndConstant.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//Variable and Constant
+//การประกาศตัวแปร แบบ Implicit
+
+var nameString = "KunSmile" //นี่คือการประกาศตัวแปรกำหนดค่าด้วย Value
+var numberInt = 123
+
+//Explicit Type
+var surnameString: String = "Tanikkool"
+var number2Int: Int = 555
+var number3Dou: Double = 12.33
+var statusBool:Bool = true
+
+
+
+
